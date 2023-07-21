a298c0547a43ae982dedb78281562022e9e52c75
Dmitrii Zolnikov <dmitri.zol@inbox.ru>
2023-07-21 16:29:46 +0300

    Asterisk configuration added

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,11 @@ apt-get update -y
 apt-get upgrade -y
 
 DEBIAN_FRONTEND=noninteractive apt-get install asterisk -y
+
+sed --in-place 's/;verbose = 3/verbose = 9/' '/etc/asterisk/asterisk.conf'
+
+cat /vagrant/asterisk/manager.conf > /etc/asterisk/manager.conf
+
+cat /vagrant/asterisk/sip.conf > /etc/asterisk/sip.conf
+
+service asterisk restart
